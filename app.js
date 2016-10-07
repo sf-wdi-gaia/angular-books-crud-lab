83b8aa509cbb18fe99c96b5b8523a255e40549ac
rhamill1 <rhamill11@gmail.com>
2016-10-07 16:25:08 -0700

    adds CRD

--- app.js
+++ app.js
@@ -36,7 +36,8 @@ function BooksIndexController ($http) {
     method: 'GET',
     url: 'https://super-crud.herokuapp.com/books'
   }).then(function successCallback(response) {
-    vm.books = response.data;
+    vm.books = response.data.books;
+    console.log(response.data.books)
   }, function errorCallback(response) {
     console.log('There was an error getting the data', response);
   });
@@ -56,7 +57,7 @@ function BooksIndexController ($http) {
   vm.editBook = function (book) {
     $http({
       method: 'PUT',
-      url: 'https://super-crud.herokuapp.com/books'+book._id,
+      url: 'https://super-crud.herokuapp.com/books/'+book._id,
       data: book
     }).then(function successCallback(json) {
       // don't need to do anything!
@@ -68,10 +69,11 @@ function BooksIndexController ($http) {
   vm.deleteBook = function (book) {
     $http({
       method: 'DELETE',
-      url: 'https://super-crud.herokuapp.com/books'+ book._id
+      url: 'https://super-crud.herokuapp.com/books/'+ book._id
     }).then(function successCallback(json) {
       var index = vm.books.indexOf(book);
-      vm.books.splice(index,1)
+      vm.books.splice(index,1);
+      console.log(index)
     }, function errorCallback(response) {
       console.log('There was an error deleting the data', response);
     });
