ea3e17155e0fb0d52c8e94fccb91ae2b2b9ec52b
lordshadowist <shadowist@live.com>
2016-10-08 16:41:33 -0700

    Add New Book Form

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,6 +8,13 @@ function LibraryController ($http)
 {
 	var vm = this;
 	vm.books = [];
+	vm.newBook = 
+	{
+		name: "",
+		author: "",
+		image: "",
+		releaseDate: ""
+	}
 	$http(
 	{
 		method: 'GET',
@@ -21,4 +28,18 @@ function LibraryController ($http)
 		{
     		console.log('There was an error getting the data', response);
   		});
+
+	vm.createBook = function()
+	{
+		$http({
+	      method: 'POST',
+	      url: 'https://super-crud.herokuapp.com/books',
+	      data: vm.newBook,
+	    }).then(function successCallback(response) {
+	      vm.books.push(response.data);
+	    }, function errorCallback(response) {
+	      console.log('There was an error posting the data', response);
+	    });
+	}
 }
+
